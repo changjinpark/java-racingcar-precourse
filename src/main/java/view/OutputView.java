e19f2a6aa04173ff96e4ec4517ef60d926171f33
changjin.park <pcjin118@gmail.com>
2021-10-11 04:40:29 +0900

    feat: 전진 값만큼 마이너스(-) 표시로 출력

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,8 +1,35 @@
 package view;
 
+import racinggame.model.Car;
+import racinggame.model.Position;
+
+import java.util.List;
+
 public class OutputView {
+    public static final String DISTANCE = "-";
+    public static final String EXECUTION_RESULT = "실행결과";
 
     public static void printErrorMessage(String e) {
             System.out.println(e);
+    }
+
+    public static void printExecuteResultText() {
+        System.out.println(EXECUTION_RESULT);
+    }
+
+    public static void printRacingResult(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.print(car.name() + " : ");
+            System.out.println(printCarDistance(car.position()));
+        }
+        System.out.println();
+    }
+
+    private static String printCarDistance(Position position) {
+        StringBuilder totalDistance = new StringBuilder();
+        for (int i = 0; i < position.distance(); i++) {
+            totalDistance.append(DISTANCE);
+        }
+        return totalDistance.toString();
     }
 }
