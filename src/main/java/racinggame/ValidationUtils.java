c56452e8184dc3c8f9790007f34996d0f73742b3
changjin.park <pcjin118@gmail.com>
2021-10-10 20:27:09 +0900

    refactor: ERROR 스트링 상수 위치 변경
    - OutputView.java -> ValidationUtils.java

--- src/main/java/racinggame/ValidationUtils.java
+++ src/main/java/racinggame/ValidationUtils.java
@@ -10,7 +10,9 @@ public class ValidationUtils {
 
     public static final int CAR_NAME_LIMIT_LENGTH = 5;
     public static final int CAR_MINIMUM_NUMBER = 2;
-    public static final int CAR_NAME_DUPLICATE = -1;
+    public static final String ERROR_MESSAGE_LENGTH = "[ERROR] 자동차 이름은 5글자 이하만 가능합니다.";
+    public static final String ERROR_MESSAGE_MINIMUM_NUMBER = "[ERROR] 자동차는 최소 2대 이상이어야 게임이 가능합니다.";
+    public static final String ERROR_MESSAGE_DUPLICATE = "[ERROR] 자동차 이름은 중복되지 않아야 게임이 가능합니다.";
 
     public static boolean validationNameLength(String[] carNameArray) {
         int i = 0;
@@ -23,7 +25,7 @@ public class ValidationUtils {
 
     private static boolean validationLength(String carName) {
         if (carName.length() > CAR_NAME_LIMIT_LENGTH){
-            printErrorMessage(CAR_NAME_LIMIT_LENGTH);
+            printErrorMessage(ERROR_MESSAGE_LENGTH);
             return false;
         }
         return true;
@@ -31,7 +33,7 @@ public class ValidationUtils {
 
     public static boolean validationNumberOfCars(String[] carNameArray) {
         if (carNameArray.length < CAR_MINIMUM_NUMBER){
-            printErrorMessage(CAR_MINIMUM_NUMBER);
+            printErrorMessage(ERROR_MESSAGE_MINIMUM_NUMBER);
             return false;
         }
         return true;
@@ -40,7 +42,7 @@ public class ValidationUtils {
     public static boolean validationNonDuplicate(List<String> carList) {
         Set<String> carSet = new HashSet<>(carList);
         if(carSet.size() != carList.size()) {
-            printErrorMessage(CAR_NAME_DUPLICATE);
+            printErrorMessage(ERROR_MESSAGE_DUPLICATE);
             return false;
         }
         return true;
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,23 +1,8 @@
 package view;
 
-import static racinggame.ValidationUtils.*;
-
 public class OutputView {
 
-    public static final String ERROR_MESSAGE_LENGTH = "[ERROR] 자동차 이름은 5글자 이하만 가능합니다.";
-    public static final String ERROR_MESSAGE_MINIMUM_NUMBER = "[ERROR] 자동차는 최소 2대 이상이어야 게임이 가능합니다.";
-    public static final String ERROR_MESSAGE_DUPLICATE = "[ERROR] 자동차 이름은 중복되지 않아야 게임이 가능합니다.";
-
-    public static void printErrorMessage(int e) {
-        if (e == CAR_NAME_LIMIT_LENGTH){
-            System.out.println(ERROR_MESSAGE_LENGTH);
-        }
-        if (e == CAR_MINIMUM_NUMBER){
-            System.out.println(ERROR_MESSAGE_MINIMUM_NUMBER);
-        }
-
-        if (e == CAR_NAME_DUPLICATE){
-            System.out.println(ERROR_MESSAGE_DUPLICATE);
-        }
+    public static void printErrorMessage(String e) {
+            System.out.println(e);
     }
 }
